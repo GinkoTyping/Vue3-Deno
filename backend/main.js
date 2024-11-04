a843dfcf9be9e0c3afc80ddb9874304910ed633f
Ginko Lee <492180240@qq.com>
2024-11-04 15:03:14 +0800

    Config cors setting.

--- backend/main.js
+++ backend/main.js
@@ -1,28 +1,30 @@
-import { Application } from 'https://deno.land/x/oak/mod.ts';
-import { Router } from "https://deno.land/x/oak/router.ts";
+import { Application, Router } from "@oak/oak";
+import { oakCors } from "@tajpouria/cors";
 
 import { initDatabase } from "./database/database.js";
 
 initDatabase();
 
-const app = new Application();
 const router = new Router();
-
+router.get('/', context => {
+  context.response.body = "Welcome to dinosaur API!";
+});
 router.get('/login', context => {
   context.response.body = 'login now';
 });
 
-// handle error
+const app = new Application();
+app.use(oakCors());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
 app.use(async (context, next) => {
   try {
     await next();
   } catch(e) {
     console.log(e.message);
   }
-})
-
-app.use(router.routes());
-app.use(router.allowedMethods());
+});
 
 app.addEventListener("listen", ({ hostname, port, secure }) => {
   console.log(
@@ -31,5 +33,4 @@ app.addEventListener("listen", ({ hostname, port, secure }) => {
     }:${port}`,
   );
 });
-
 app.listen({ port: 8000 });
